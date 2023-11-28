1807760d8e98c41ccd5534720a4e307da3f6c49c
26june <gj.abordo1@gmail.com>
2023-11-28 10:07:30 +0000

    create comment form

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,1 +1,18 @@
-export default function Comments() {}
+export default async function Comments({ postSlug }: { postSlug: string }) {
+  return (
+    <div>
+      <h2>Comments</h2>
+      <h3>Leave a comment below...</h3>
+
+      <form action={`/api/comments/${postSlug}`} method="POST">
+        <label htmlFor="username">Name</label>
+        <input type="text" name="username" />
+
+        <label htmlFor="comment">Your Comment</label>
+        <textarea name="comment" cols={30} rows={10}></textarea>
+
+        <input type="submit" value={"Send Comment"} />
+      </form>
+    </div>
+  );
+}
