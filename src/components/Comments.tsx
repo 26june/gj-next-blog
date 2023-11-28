d4168c2e1970779094e505d0e082a0b2a0dac922
26june <gj.abordo1@gmail.com>
2023-11-28 15:03:31 +0000

    show comments

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,4 +1,20 @@
 export default async function Comments({ postSlug }: { postSlug: string }) {
+  const WEBSITE_URL = "http://localhost:3000";
+
+  let comments = [];
+
+  try {
+    const commentsResult = await fetch(
+      `${WEBSITE_URL}/api/comments/${postSlug}`,
+      { next: { revalidate: 5 } }
+    );
+    const response = await commentsResult.json();
+    console.log(response);
+    comments = response.comments.rows;
+  } catch (err) {
+    console.log(err);
+  }
+
   return (
     <div>
       <h2>Comments</h2>
@@ -22,6 +38,18 @@ export default async function Comments({ postSlug }: { postSlug: string }) {
 
         <input type="submit" value={"Send Comment"} />
       </form>
+
+      {comments.map(
+        (comment: { id: string; username: string; comment: string }) => {
+          return (
+            <li key={comment.id}>
+              {comment.username} says...
+              <br />
+              {comment.comment}
+            </li>
+          );
+        }
+      )}
     </div>
   );
 }
