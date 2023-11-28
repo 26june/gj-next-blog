65b723899b43f805dcd7bccfa14e9bd6f81d80d1
26june <gj.abordo1@gmail.com>
2023-11-28 13:06:48 +0000

    handle route

---
+++ src/app/api/comments/[slug]/route.ts
@@ -0,0 +1,26 @@
+import { getComments, saveComments } from "@/lib/comments";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { slug: string } }
+) {
+  const { slug } = params;
+  const comments = await getComments(slug);
+  return NextResponse.json({ comments });
+}
+
+export async function POST(
+  request: NextRequest,
+  { params }: { params: { slug: string } }
+) {
+  const { slug } = params;
+
+  const formData = await request.formData();
+  const username = formData.get("username") as string;
+  const content = formData.get("content") as string;
+
+  await saveComments(username, content, slug);
+
+  return NextResponse.json("comment saved!");
+}
