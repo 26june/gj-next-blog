fefb1f63429b5ddc60b50688da05a169197e8a2e
26june <gj.abordo1@gmail.com>
2023-11-28 13:06:40 +0000

    create blog page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,1 +1,21 @@
-export default function BlogPage() {}
+import { getPosts } from "@/lib/posts";
+import Link from "next/link";
+
+export default function BlogPage() {
+  const posts = getPosts();
+
+  return (
+    <div>
+      <h2>My Posts:</h2>
+      <ul>
+        {posts.map((e) => {
+          return (
+            <li key={e.slug}>
+              <Link href={`blog/${e.slug}`}>{e.title}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
